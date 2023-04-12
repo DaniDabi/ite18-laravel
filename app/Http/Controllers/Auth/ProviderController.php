ff48cbe6aa04ce5be5275c59140ebe8261772afb
Danedave Corpin <danedave.silva@gmail.com>
2023-04-12 09:26:21 +0800

    first commit

---
+++ app/Http/Controllers/Auth/ProviderController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+
+class ProviderController extends Controller
+{
+    public function redirect($provider){
+        return Socialite::driver($provider)->redirect();
+
+    }
+    public function callback($provider){
+        $SocialUser = Socialite::driver($provider)->user();
+
+        $user = User::updateOrCreate([
+        'provider_id' => $SocialUser->id,
+        'provider'=> $provider
+    ], [
+        'name' => $SocialUser->name,
+        'email' => $SocialUser->email,
+        'password'=>$SocialUser->token,
+        'provider_token' => $SocialUser->token,
+    ]);
+ 
+    Auth::login($user);
+ 
+    return redirect('/dashboard');
+    }
+}
